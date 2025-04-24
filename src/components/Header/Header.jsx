b5facce1dbe7f643ac484964b7b4c6e0fd168661
Daud <daud_edilbiev@mail.ru>
2025-04-24 19:24:06 +0400

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { formatDate } from "../../helpers/formateDate";
+
+import styles from "./styles.module.css";
+
+const Header = () => {
+	return (
+		<header className={styles.header}>
+			<h1 className={styles.title}>Kerlanash</h1>
+			<p className={styles.date}>{formatDate(new Date())}</p>
+		</header>
+	);
+};
+
+export default Header;
